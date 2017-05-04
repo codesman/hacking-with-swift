d199de1c6ec7c3e3336116f270392e1c558adcca
Tom Holland <codesman@hackattic.net>
2017-05-04 11:51:06 -0700

    TableView Setup

--- WordGame/WordGame/ViewController.swift
+++ WordGame/WordGame/ViewController.swift
@@ -40,7 +40,18 @@ class ViewController: UITableViewController {
         tableView.reloadData()
         
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return usedWords.count
+    }
 
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Word", for: indexPath)
+        
+        cell.textLabel?.text = usedWords[indexPath.row]
+        
+        return cell
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
