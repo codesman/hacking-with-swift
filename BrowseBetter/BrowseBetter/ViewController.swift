df78fced76d94164d5c010da44875804747ace42
Tom Holland <codesman@hackattic.net>
2017-05-04 10:45:13 -0700

    Refactor & check allowed

--- BrowseBetter/BrowseBetter/ViewController.swift
+++ BrowseBetter/BrowseBetter/ViewController.swift
@@ -13,13 +13,14 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     var webView: WKWebView!
     var progressView: UIProgressView!
+    var sites = ["apple.com", "hackingwithswift.com"]
     
     override func loadView() {
         webView = WKWebView()
         webView.navigationDelegate = self
         view = webView
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -37,7 +38,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
         toolbarItems = [progressButton, spacer, refresh]
         navigationController?.isToolbarHidden = false
         
-        if let url = URL(string: "https://www.hackingwithswift.com") {
+        if let url = URL(string: "https://" + sites[0]) {
             webView.load(URLRequest(url: url))
             webView.allowsBackForwardNavigationGestures = true
         }
@@ -48,33 +49,49 @@ class ViewController: UIViewController, WKNavigationDelegate {
             progressView.progress = Float(webView.estimatedProgress)
         }
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
     func openTapped() {
         let alert = UIAlertController(title: "Open Page...", message: nil, preferredStyle: .actionSheet)
         
-        alert.addAction(UIAlertAction(title: "apple.com", style: .default, handler: openPage))
-        alert.addAction(UIAlertAction(title: "hackingwithswift.com", style: .default, handler: openPage))
+        for site in sites {
+            alert.addAction(UIAlertAction(title: site, style: .default, handler: openPage))
+        }
+        
         alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
         
-//        alert.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
-    
+        //        alert.popoverPresentationController?.barButtonItem = self.navigationItem.rightBarButtonItem
+        
         present(alert, animated:  true)
     }
     
     func openPage(action: UIAlertAction!) {
         guard let title = action.title, let url = URL(string: "https://" + title) else { return }
         
-         webView.load(URLRequest(url: url))
+        webView.load(URLRequest(url: url))
     }
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
         title = webView.title
+    }
+    
+    func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+        
+        guard let host = navigationAction.request.url?.host else { return }
+        
+        for site in sites {
+            if host.range(of: site) != nil {
+                decisionHandler(.allow)
+                return
+            }
+        }
+        
+        decisionHandler(.cancel)
     }
 }
 
