8bc3ae97d0ee64f6c7272f47c04c80162e2f2d39
Tom Holland <codesman@hackattic.net>
2017-05-05 13:47:43 -0700

    AutoLayout - Initial Commit

---
+++ AutoLayout/AutoLayout/ViewController.swift
@@ -0,0 +1,60 @@
+//
+//  ViewController.swift
+//  AutoLayout
+//
+//  Created by Tom Holland on 5/5/17.
+//  Copyright © 2017 Tom Holland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let labelOne = UILabel()
+        labelOne.translatesAutoresizingMaskIntoConstraints = false
+        labelOne.backgroundColor = UIColor.red
+        labelOne.text = " THESE"
+        
+        let labelTwo = UILabel()
+        labelTwo.translatesAutoresizingMaskIntoConstraints = false
+        labelTwo.backgroundColor = UIColor.cyan
+        labelTwo.text = " ARE"
+        
+        let labelThree = UILabel()
+        labelThree.translatesAutoresizingMaskIntoConstraints = false
+        labelThree.backgroundColor = UIColor.yellow
+        labelThree.text = " SOME"
+        
+        let labelFour = UILabel()
+        labelFour.translatesAutoresizingMaskIntoConstraints = false
+        labelFour.backgroundColor = UIColor.green
+        labelFour.text = " AWESOME"
+        
+        let labelFive = UILabel()
+        labelFive.translatesAutoresizingMaskIntoConstraints = false
+        labelFive.backgroundColor = UIColor.orange
+        labelFive.text = " LABELS"
+        
+        
+        view.addSubview(labelOne)
+        view.addSubview(labelTwo)
+        view.addSubview(labelThree)
+        view.addSubview(labelFour)
+        view.addSubview(labelFive)
+    }
+
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
