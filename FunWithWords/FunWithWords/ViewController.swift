192c14ea4b815db68d75c67b77b712624fb2afec
Tom Holland <codesman@hackattic.net>
2017-05-08 07:14:59 -0700

    Add Actions

--- FunWithWords/FunWithWords/ViewController.swift
+++ FunWithWords/FunWithWords/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var cluesLabel: UILabel!
+    @IBOutlet var answersLabel: UILabel!
+    @IBOutlet var currentAnswer: UITextField!
+    @IBOutlet var scoreLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
+    @IBAction func submitTapped(_ sender: Any) {
+    }
+    @IBOutlet var clearTapped: UIButton!
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
