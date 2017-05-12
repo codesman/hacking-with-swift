19b2635e81355c8fd720bbd27e12970f6870e657
Tom Holland <codesman@hackattic.net>
2017-05-12 10:32:29 -0700

    Initial Commit - NamesToFaces

---
+++ NamesToFaces/NamesToFaces/PersonCell.swift
@@ -0,0 +1,16 @@
+//
+//  PersonCell.swift
+//  NamesToFaces
+//
+//  Created by Tom Holland on 5/12/17.
+//  Copyright © 2017 Tom Holland. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var name: UILabel!
+    
+}
---
+++ NamesToFaces/NamesToFaces/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NamesToFaces
+//
+//  Created by Tom Holland on 5/12/17.
+//  Copyright © 2017 Tom Holland. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UICollectionViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
