312b461f65f8e74671fcbbe548ce5ccd97df78a9
Tom Holland <codesman@hackattic.net>
2017-05-13 08:18:37 -0700

    Add Physics

--- Pachinko/Pachinko/GameScene.swift
+++ Pachinko/Pachinko/GameScene.swift
@@ -12,14 +12,30 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
+        
         let background = SKSpriteNode(imageNamed: "background.jpg")
+        
         background.position = CGPoint(x: 521, y: 384)
         background.blendMode = .replace
         background.zPosition = -1
+        
         addChild(background)
+        
+        physicsBody = SKPhysicsBody(edgeLoopFrom: frame)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        
+        if let touch = touches.first {
+            
+            let location = touch.location(in: self)
+            let box = SKSpriteNode(color: UIColor.red, size: CGSize(width: 64, height: 64))
+            
+            box.physicsBody = SKPhysicsBody(rectangleOf: CGSize(width: 64, height: 64))
+            box.position = location
+            
+            addChild(box)
+        }
         
     }
     
