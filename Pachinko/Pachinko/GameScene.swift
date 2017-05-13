b2fbdc59018d3770f370c33a54a9f050e0fcd277
Tom Holland <codesman@hackattic.net>
2017-05-13 08:11:07 -0700

    Add background

--- Pachinko/Pachinko/GameScene.swift
+++ Pachinko/Pachinko/GameScene.swift
@@ -12,7 +12,11 @@ import GameplayKit
 class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
-        
+        let background = SKSpriteNode(imageNamed: "background.jpg")
+        background.position = CGPoint(x: 521, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
