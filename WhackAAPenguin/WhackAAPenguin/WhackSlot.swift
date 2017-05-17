061d3871fac2f00ea5c6ba28ab4e638996bd81bf
Tom Holland <codesman@hackattic.net>
2017-05-17 09:21:32 -0700

    Add WhackSlot

---
+++ WhackAAPenguin/WhackAAPenguin/WhackSlot.swift
@@ -0,0 +1,14 @@
+//
+//  WhackSlot.swift
+//  WhackAAPenguin
+//
+//  Created by Tom Holland on 5/17/17.
+//  Copyright © 2017 Tom Holland. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class WhackSlot: SKSpriteNode {
+
+}
