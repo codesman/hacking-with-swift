6514cac6a4361cf84016bb054ce2ed235a50bece
Tom Holland <codesman@hackattic.net>
2017-05-17 09:31:13 -0700

    Mask Penguins

--- WhackAAPenguin/WhackAAPenguin/WhackSlot.swift
+++ WhackAAPenguin/WhackAAPenguin/WhackSlot.swift
@@ -11,4 +11,26 @@ import SpriteKit
 
 class WhackSlot: SKSpriteNode {
 
+    var characterNode: SKSpriteNode!
+    
+    func configure(at position: CGPoint) {
+        
+        self.position = position
+        
+        let sprite = SKSpriteNode(imageNamed: "whackHole")
+        
+        addChild(sprite)
+        
+        let cropNode = SKCropNode()
+        cropNode.position = CGPoint(x: 0, y: 15)
+        cropNode.zPosition = 1
+        cropNode.maskNode = SKSpriteNode(imageNamed: "whackMask")
+        
+        characterNode = SKSpriteNode(imageNamed: "penguinGood")
+        characterNode.position = CGPoint(x: 0, y: -90)
+        characterNode.name = "character"
+        cropNode.addChild(characterNode)
+        
+        addChild(cropNode)
+    }
 }
