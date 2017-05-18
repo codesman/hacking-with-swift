6ec13d3e057b121200a08dcbedc592a530dc9a37
Tom Holland <codesman@hackattic.net>
2017-05-18 09:15:09 -0700

    Animation setup

--- Animation/Animation/ViewController.swift
+++ Animation/Animation/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     var imageView: UIImageView!
     var currentAnimation = 0
     
@@ -17,19 +17,31 @@ class ViewController: UIViewController {
     
     @IBAction func tapped(_ sender: Any) {
         
+        tap.isHidden = true
+        
+        UIView.animate(
+            withDuration: 1,
+            delay: 0,
+            options: [],
+            animations: { [unowned self] in self.switchAnimation() },
+            completion: { [unowned self] (finished: Bool) in self.tap.isHidden = false }
+        )
+    }
+    
+    func switchAnimation() {
+        switch self.currentAnimation {
+        case 0:
+            break
+        default:
+            break
+        }
+        
         currentAnimation += 1
         
         guard currentAnimation < 8 else {
             
             currentAnimation = 0
             return
-        }
-        
-        switch currentAnimation {
-        case 1:
-            break
-        default:
-            break
         }
     }
     
@@ -40,7 +52,7 @@ class ViewController: UIViewController {
         imageView.center = CGPoint(x: 512, y: 384)
         view.addSubview(imageView)
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
