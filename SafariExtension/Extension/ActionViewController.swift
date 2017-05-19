872fd602e970c23e8be441974f633b6e321be8bf
Tom Holland <codesman@hackattic.net>
2017-05-19 09:03:00 -0700

    Add Extension

---
+++ SafariExtension/Extension/ActionViewController.swift
@@ -0,0 +1,34 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Tom Holland on 5/19/17.
+//  Copyright © 2017 Tom Holland. All rights reserved.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+        
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func done() {
+        // Return any edited content to the host app.
+        // This template doesn't do anything, so we just echo the passed in items.
+        self.extensionContext!.completeRequest(returningItems: self.extensionContext!.inputItems, completionHandler: nil)
+    }
+
+}
