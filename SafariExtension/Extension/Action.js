06afaa45f0a2a2984c0bf3a8cc9df8b86c825f83
Tom Holland <codesman@hackattic.net>
2017-05-19 09:16:34 -0700

    WIP Javascript

--- SafariExtension/Extension/Action.js
+++ SafariExtension/Extension/Action.js
@@ -3,11 +3,12 @@ var Action = function() {};
 Action.prototype = {
 run: function(parameters) {
     
+    parameters.completionFunction({"URL": document.URL, "title": document.title});
 },
 finalize: function() {
-    
+    var customJavaScript = parameters["customJavaScript"];
+    eval(customJavaScript);
 }
-    
 };
 
 var ExtensionPreprocessingJS = new Action
