816484003dc9404cae7e04448b1f87458e97c32a
Tom Holland <codesman@hackattic.net>
2017-05-25 07:26:42 -0700

    Create Slices

--- SwiftyNinja/SwiftyNinja/GameScene.swift
+++ SwiftyNinja/SwiftyNinja/GameScene.swift
@@ -11,10 +11,80 @@ import GameplayKit
 
 class GameScene: SKScene {
     
+    var gameScore: SKLabelNode!
+    var score: Int = 0 {
+        didSet {
+            gameScore.text = "Score: \(score)"
+        }
+    }
+    var livesImages = [SKSpriteNode]()
+    var lives = 3
+    
+    var activeSliceBG: SKShapeNode!
+    var activeSliceFG: SKShapeNode!
+    
+    
     override func didMove(to view: SKView) {
-
+        
+        setBackground()
+        setPhysics()
+        
+        createScore()
+        createLives()
+        createSlices()
+        
     }
     
+    func createScore() {
+        gameScore = SKLabelNode(fontNamed: "Chalkduster")
+        gameScore.text = "Score: 0"
+        gameScore.horizontalAlignmentMode = .left
+        gameScore.fontSize = 48
+        gameScore.position = CGPoint(x: 8, y: 8)
+        
+        addChild(gameScore)
+    }
+    
+    func createLives() {
+        for i in 0 ..< 3 {
+            let spriteNode = SKSpriteNode(imageNamed: "sliceLife")
+            spriteNode.position = CGPoint(x: CGFloat(834 + (i * 70)), y: 720)
+            
+            addChild(spriteNode)
+            
+            livesImages.append(spriteNode)
+        }
+    }
+    
+    func createSlices() {
+        activeSliceBG = SKShapeNode()
+        activeSliceBG.zPosition = 2
+        activeSliceBG.strokeColor = UIColor(red: 1, green: 0.9, blue: 0, alpha: 1)
+        activeSliceBG.lineWidth = 9
+        
+        activeSliceFG = SKShapeNode()
+        activeSliceFG.zPosition = 2
+        activeSliceFG.strokeColor = UIColor.white
+        activeSliceFG.lineWidth = 5
+        
+        addChild(activeSliceBG)
+        addChild(activeSliceFG)
+    }
+    
+    func setBackground() {
+        let background = SKSpriteNode(imageNamed: "sliceBackground")
+        
+        background.position = CGPoint(x: 512, y: 384)
+        background.blendMode = .replace
+        background.zPosition = -1
+        addChild(background)
+        
+    }
+    
+    func setPhysics() {
+        physicsWorld.gravity = CGVector(dx: 0, dy: -6)
+        physicsWorld.speed = 0.85
+    }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         
